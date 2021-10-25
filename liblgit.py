43fbc5133e86f245ad9bba133ba1c1998c94d572
Bohdan <bohdan.kornatskyi@gmail.com>
2021-10-24 19:29:24 -0700

    init classes:

--- liblgit.py
+++ liblgit.py
@@ -1,2 +1,50 @@
-print("hello git")
-print("hello git")
+import os
+
+print(os.listdir("./"))
+
+
+class File:
+    def __init__(self, name, content=None):
+        self.name = name
+        self.content = content
+        pass
+
+
+class Dir:
+    def __init__(self, name, files):
+        self.name = name
+        self.files = files
+        pass
+
+
+class Saving:
+    def __init__(self, name):
+        self.name = name
+        pass
+
+
+class DotDir:
+    def __init__(self):
+        self.dir_name = '.lgit'
+        pass
+
+    # Initialize a new .lgit directory
+    def init(self):
+        if os.path.isdir('./.lgit'):
+            print(".lgit directory already exists")
+        else:
+            os.mkdir(path='./' + self.dir_name)
+            os.mkdir(path='./' + self.dir_name + '/' + "savings")
+            print("New .lgit is initialized!")
+
+
+def main():
+    print()
+    dot_dir = DotDir()
+    dot_dir.init()
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
